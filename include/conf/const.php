02c2425ee8bf8990e4269c6d06413a3d8e6695bb
町田祐介 <yusukemachita@machidayuusukenoMacBook-Air.local>
2020-10-19 23:48:51 +0900

    edit conf

--- include/conf/const.php
+++ include/conf/const.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST',   'localhost'); // データベースのホスト名又はIPアドレス
-define('DB_USER',   'root');  // MySQLのユーザ名
-define('DB_PASSWD', 'root');    // MySQLのパスワード
-define('DB_NAME',   'KINJO');    // データベース名
+define('DB_HOST',   'us-cdbr-east-02.cleardb.com'); // データベースのホスト名又はIPアドレス
+define('DB_USER',   'bc3bf92d26b5cf');  // MySQLのユーザ名
+define('DB_PASSWD', '34c5268e');    // MySQLのパスワード
+define('DB_NAME',   'heroku_477065a0d79d439');    // データベース名
  
 define('HTML_CHARACTER_SET', 'UTF-8');  // HTML文字エンコーディング
 define('DB_CHARACTER_SET',   'UTF8');   // DB文字エンコーディング
