ee5188a2ccc74b9e6bcf3bd4c49996b32f2eb8cc
町田祐介 <yusukemachita@machidayuusukenoMacBook-Air.local>
2020-10-20 18:40:21 +0900

    css change

--- include/view/view_login.php
+++ include/view/view_login.php
@@ -5,7 +5,7 @@
       <title>KINJO ログイン</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       
-      <link rel="stylesheet" href="kinjo.css">
+      <link rel="stylesheet" href="../kinjo.css">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   </head>
@@ -35,7 +35,7 @@
     </header>
     <div class="container-fluid">
       <div>
-        <img src="./bruce-mars-HHXdPG_eTIQ-unsplash.jpg" alt="" class="top_img">
+        <img src="../bruce-mars-HHXdPG_eTIQ-unsplash.jpg" alt="" class="top_img">
         </div>
       <!--ログイン-->
       <div class="input_container">
